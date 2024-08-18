1f472f2d7cdb286bdbe3f91ef4716107f31b1f0a
AshleyJohnson90 <asrejohn@gmail.com>
2024-08-18 19:20:52 -0400

    updated CoursesPage.Server

--- CoursesPage.Server/Controllers/TableController.cs
+++ CoursesPage.Server/Controllers/TableController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CoursesPage.Server.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
